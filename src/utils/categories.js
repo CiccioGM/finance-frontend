685eeb7b6807adb5819b98676555126846798a0c
CiccioGM <ilqudi88@gmail.com>
2025-11-10 17:56:33 +0100

    Create categories.js

---
+++ src/utils/categories.js
@@ -0,0 +1,8 @@
+export const categories = [
+  { name: "Cibo", icon: "🍕" },
+  { name: "Trasporti", icon: "🚌" },
+  { name: "Shopping", icon: "🛍️" },
+  { name: "Casa", icon: "🏠" },
+  { name: "Svago", icon: "🎮" },
+  { name: "Altro", icon: "💸" },
+];
