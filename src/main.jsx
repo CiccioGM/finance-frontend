d91b2d69dc909c60d8bb84f2e6c255a8eaa9d8da
CiccioGM <ilqudi88@gmail.com>
2025-11-10 21:25:00 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
+import { TransactionsProvider } from "./context/TransactionsContext";
 import "../styles.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TransactionsProvider>
+      <App />
+    </TransactionsProvider>
+  </React.StrictMode>
+);
