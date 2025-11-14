5604ed6b8dd82a186378db300e9addb124952e07
CiccioGM <ilqudi88@gmail.com>
2025-11-14 14:31:47 +0100

    Update MonthlyBarChart.jsx

--- src/components/MonthlyBarChart.jsx
+++ src/components/MonthlyBarChart.jsx
@@ -1,17 +1,41 @@
 import React from "react";
-import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend } from "recharts";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  Legend,
+} from "recharts";
 
 export default function MonthlyBarChart({ data }) {
   return (
     <div style={{ width: "100%", height: 280 }}>
       <ResponsiveContainer>
-        <BarChart data={data} margin={{ top: 20, right: 20, left: 0, bottom: 5 }}>
+        <BarChart
+          data={data}
+          margin={{ top: 20, right: 20, left: 0, bottom: 5 }}
+          barCategoryGap="30%"
+          barGap={0}
+        >
           <XAxis dataKey="label" />
           <YAxis />
-          <Tooltip formatter={(v)=>v.toFixed(2)} />
+          <Tooltip formatter={(v) => v.toFixed(2)} />
           <Legend />
-          <Bar dataKey="entrate" name="Entrate" stackId="a" barSize={18} />
-          <Bar dataKey="uscite" name="Uscite" stackId="a" barSize={18} />
+          {/* Entrate in verde, uscite in rosso, affiancate */}
+          <Bar
+            dataKey="entrate"
+            name="Entrate"
+            fill="#16a34a" // verde
+            barSize={18}
+          />
+          <Bar
+            dataKey="uscite"
+            name="Uscite"
+            fill="#dc2626" // rosso
+            barSize={18}
+          />
         </BarChart>
       </ResponsiveContainer>
     </div>
