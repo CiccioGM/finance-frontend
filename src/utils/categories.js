21d9f6440c940c1383a4937416030f79708103e6
CiccioGM <ilqudi88@gmail.com>
2025-11-14 12:29:47 +0100

    Update categories.js

--- src/utils/categories.js
+++ src/utils/categories.js
@@ -1,8 +1,8 @@
-export const categories = [
-  { name: "Cibo", icon: "🍕" },
-  { name: "Trasporti", icon: "🚌" },
-  { name: "Shopping", icon: "🛍️" },
-  { name: "Casa", icon: "🏠" },
-  { name: "Svago", icon: "🎮" },
-  { name: "Altro", icon: "💸" },
+export const defaultCategories = [
+  { name: "Stipendio", type: "entrata", icon: "💵", color: "#00C49F" },
+  { name: "Cibo", type: "uscita", icon: "🍕", color: "#FF8042" },
+  { name: "Trasporti", type: "uscita", icon: "🚗", color: "#FFBB28" },
+  { name: "Affitto", type: "uscita", icon: "🏠", color: "#0088FE" },
+  { name: "Intrattenimento", type: "uscita", icon: "🎮", color: "#AA66CC" },
+  { name: "Altro", type: "uscita", icon: "💸", color: "#AAAAAA" }
 ];
