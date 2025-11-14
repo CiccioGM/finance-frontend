053fee38bf4c64d746716153b995efd9516d2d00
CiccioGM <ilqudi88@gmail.com>
2025-11-14 13:15:56 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // optional: reduce chunk size warnings
-    chunkSizeWarningLimit: 1200
-  }
+  build: { sourcemap: true }
 });
